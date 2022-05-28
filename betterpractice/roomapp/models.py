64d2637ce7861441db0c8b8aeb7f3e16639d2031
Peter Zhong <zhongyong5@gmail.com>
2022-05-28 14:39:23 -0500

    push checkout time along with starting covid notification scheme

--- betterpractice/roomapp/models.py
+++ betterpractice/roomapp/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+import datetime
+
 
 
 class User(models.Model):
-    netid = models.CharField(max_length=200)
+    netid = models.CharField(max_length=200, primary_key=True)
     email = models.EmailField()
 
-
+    @staticmethod
+    def get_netid_from_token(token):
+        return token
 
 class PracticeRoom(models.Model):
     room_name = models.CharField(max_length=200)
@@ -15,3 +19,4 @@ class Checkin(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(PracticeRoom, on_delete=models.CASCADE)
     checkin_time = models.DateTimeField()
+    checkout_time = models.DateTimeField()
--- betterpractice/roomapp/views.py
+++ betterpractice/roomapp/views.py
@@ -2,7 +2,32 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import User
+from .models import Checkin
+from datetime import datetime, timedelta
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def notifyCovid(request):
+    if request.method != 'POST':
+        return HttpResponse(status=400)
+    user_token = request.headers['Authorization']
+    user_netid = User.get_netid_from_token(user_token)
+    try:
+        actualUser = User.objects.get(netid__exact=user_netid)
+    except User.DoesNotExist:
+        return HttpResponse(status=404)
+    covid_period = 72
+    last_date = datetime.now() - timedelta(hours=covid_period)
+    check_ins = Checkin.objects.filter(user=actualUser, checkin_time__range=(last_date, datetime.now())).query
+    print(check_ins)
+    rooms = []
+
+    return HttpResponse(actualUser.email)
