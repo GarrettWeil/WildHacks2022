899e1c24e38f474e9baea21a79b342fd04d90974
Peter Zhong <zhongyong5@gmail.com>
2022-05-29 00:51:37 -0500

    push this shit

--- betterpractice/roomapp/views.py
+++ betterpractice/roomapp/views.py
@@ -70,6 +70,10 @@ def checkin(request):
         return HttpResponse(status=400)
     user_token = request.headers['Authorization']
     user_netid = User.get_netid_from_token(user_token)
+    data = json.loads(request.body.decode('utf-8'))
+
+    if 'email' in data and data['email'] is not None and len(data['email']) > 0:
+        User.objects.create(netid=user_netid, email=data['email'])
     try:
         actualUser = User.objects.get(netid__exact=user_netid)
     except User.DoesNotExist:
@@ -77,7 +81,7 @@ def checkin(request):
 
     checkin_period = 4
 
-    room = PracticeRoom.objects.get(room_name__exact=request.POST['room'])
+    room = PracticeRoom.objects.get(room_name__exact=data['room'])
     last_checkout = Checkin.objects.filter(room=room).order_by('-checkout_time').first()
 
     if last_checkout is not None and last_checkout.checkout_time > timezone.now():
@@ -85,7 +89,7 @@ def checkin(request):
         last_checkout.save()
 
     new_checkin = Checkin.objects.create(user=actualUser, room=room, checkin_time=timezone.now(),
-                                         checkout_time=timezone.now()+ timedelta(hours=checkin_period))
+                                         checkout_time=timezone.now() + timedelta(hours=checkin_period))
 
     return HttpResponse("Checkin request sent")
 
@@ -115,19 +119,18 @@ def redirect_(request, room):
         return HttpResponse(status=400)
     netid = request.POST['callback_0']
     userexist = User.objects.filter(netid=netid).exists()
-    return redirect(f"/static/main.html?userexist={userexist}&token={netid}&room={room}")
+    return redirect(f"/static/checkin.html?userexist={userexist}&token={netid}&room={room}")
 
 
 @csrf_exempt
 def bulk_room_status(request):
-
     rooms_raw = request.body.decode('utf-8')
     rooms = json.loads(rooms_raw)
-    practiceRooms = PracticeRoom.objects.filter(room_name__in=rooms)
+    # practiceRooms = PracticeRoom.objects.filter(room_name__in=rooms)
     checkins = Checkin.objects.values("room__room_name").annotate(time=Max('checkout_time'))
     room_status = {}
     for room in rooms:
-        room_status[room] = datetime(2002, 5,11)
+        room_status[room] = datetime(2002, 5, 11)
     for checkin in checkins:
         room_status[checkin['room__room_name']] = checkin['time']
 
@@ -143,11 +146,6 @@ def bulk_room_status(request):
     # from django.core.serializers.json import DjangoJSONEncoder
     # print(json.dumps(room_status, cls=DjangoJSONEncoder))
     # return HttpResponse('{"room1": "2022-05-30T00:10:02.310Z", "room2": "2022-05-30T00:10:24.552Z", "room3": "2022-05-30T00:10:33.491Z", "room4": "NeverBooked", "room5": "NeverBooked", "room6": "NeverBooked", "room7": "NeverBooked", "room8": "NeverBooked", "room9": "NeverBooked", "room10": "NeverBooked", "room11": "NeverBooked", "room12": "NeverBooked", "room13": "NeverBooked", "room14": "NeverBooked", "room15": "NeverBooked", "room16": "NeverBooked", "room17": "NeverBooked", "room18": "NeverBooked", "room19": "NeverBooked", "room20": "NeverBooked", "room21": "NeverBooked", "room22": "NeverBooked", "room23": "NeverBooked", "room24": "NeverBooked", "room25": "NeverBooked"}')
-
-
-
-
-
 
 # Checkin.objects.values("room__room_name").annotate(time=Max('checkout_time')).first()
 
--- betterpractice/static/mapview.js
+++
@@ -1,6 +0,0 @@
-mport React, { useRef, useEffect, useState } from 'react';
-import './mapview.css';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello, world!</h1>);
